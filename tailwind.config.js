7b31a1be682fb1cbb25ecdba90875a975435341e
Sathish G <inihsathish@gmail.com>
2023-07-07 16:35:37 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+import forms from "@tailwindcss/forms";
+import typography from "@tailwindcss/typography";
+
+/* @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./vendor/protonemedia/laravel-splade/lib/**/*.vue",
+        "./vendor/protonemedia/laravel-splade/resources/views/**/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.vue",
+        // "./app/Forms/*.php",
+        // "./app/Tables/*.php",
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans1: ["Comme"],
+                sans11: ["Kalam"],
+                sans: ["Spectral SC"]
+            },
+        },
+    },
+
+    plugins: [forms, typography],
+};
